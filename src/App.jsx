1b9892c120f2828d5d87f434c858ef3a85db9c2d
tibex88 <tibesolomon7@gmail.com>
2025-03-11 14:32:16 +0300

    add: dynamically render locus zoom

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
+// import React, { useState } from "react";
+// import LZoom from "./LZoom";
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LZoom from './components/locuszoom'
-// import LZoomLocal from './components/lzLocal'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [start, setStart] = useState(53673256);
+  const [end, setEnd] = useState(53831146);
+  const [variant, setVariant] = useState(""); // Adjust this if you want to use the variant prop
+
+  const handleStartChange = (e) => setStart(Number(e.target.value));
+  const handleEndChange = (e) => setEnd(Number(e.target.value));
+  const handleVariantChange = (e) => setVariant(e.target.value);
 
   return (
-    <>
-      <LZoom />
-      {/* <LZoomLocal /> */}
-    </>
-  )
-}
+    <div>
+      <h1>LocusZoom Plot</h1>
+      
+      <label>
+        Start: 
+        <input type="number" value={start} onChange={handleStartChange} />
+      </label>
 
-export default App
+      <label>
+        End: 
+        <input type="number" value={end} onChange={handleEndChange} />
+      </label>
+
+      <label>
+        Variant: 
+        <input type="text" value={variant} onChange={handleVariantChange} />
+      </label>
+
+      <LZoom start={start} end={end} variant={variant} />
+    </div>
+  );
+};
+
+export default App;
--- src/components/locuszoom.jsx
+++ src/components/locuszoom.jsx
@@ -1,106 +1,79 @@
 import React, { useEffect, useRef } from "react";
 import 'locuszoom/dist/locuszoom.css';
+import './../../public/style.css'
 import LocusZoom from 'locuszoom'
-import json from './../assets/data/genes.json'
-const LZoom = () => {
+
+const LZoom = ({ start = 53673256, end = 53831146, variant }) => {
 
   const plotRef = useRef(null);
   const inputRef = useRef(null);
+  const rangeDifference = end - start; // Calculate the difference
+  const namespace = "cojo"
 
   useEffect(() => {
-    const baseUrl = "http://localhost:8000/"
-    // const baseUrl = "http://portaldev.sph.umich.edu/"
-    // const baseUrl = "http://dl.tail9c350.ts.net/"
 
     // Create the LocusZoom plot
     if (plotRef.current) {
-      const genes_data = [
-        { gene_name: "BRCA1", gene_id: "ENSG00000012048.1", chrom: "17", start: 43044295, end: 43125482 },
-        { gene_name: "TP53", gene_id: "ENSG00000141510.1", chrom: "17", start: 7668402, end: 7687550 },
-        { gene_name: "Y_RNA", gene_id: "ENSG00000207243.1", chrom: "16", start: 228553, end: 228655 },
-      ];
+      const baseUrl = "http://100.67.47.42:5011/json/"
 
       // Data Sources - APIs LocusZoom will connect to for various pieces of data
-    var data_sources = new LocusZoom.DataSources()
-    .add("sig", ["StaticJSON", { data: [{ "x": 0, "y": 7.30103 }, { "x": 2881033286, "y": 7.30103 }] }])
-    .add("gene", ["GeneLZ", { 
-      // url: "http://localhost:3000/gene/",
-      url: `${baseUrl}genes/`, 
-      // build: 'GRCh38'
-    }])
-    .add("constraint", ["GeneConstraintLZ", {
-      url: "http://exac.broadinstitute.org/api/constraint",
-    }])
-    .add("recomb", ["RecombLZ", {
-      // url: "http://localhost:3000/recomb/"
-      url: `${baseUrl}/recomb`, 
-      } 
-    ])
-    .add("ld", ["LDServer", { 
-      // url: "http://localhost:3000/ld/" 
-      url: "https://portaldev.sph.umich.edu/ld/" ,
-      variant: "16:53668214:T:G",
-      }])
-    .add("study_41", ["AssociationLZ", { 
-      // url: "http://localhost:3000/single/", 
-      url: `${baseUrl}single/`, 
-    }]
-)
+      var data_sources = new LocusZoom.DataSources()
+        .add("sig", ["StaticJSON", { data: [{ "x": 0, "y": 7.30103 }, { "x": 2881033286, "y": 7.30103 }] }])
+        .add("gene", ["GeneLZ", {
+          url: "http://portaldev.sph.umich.edu/api/v1/annotation/genes/", 
+        }])
+        .add("constraint", ["GeneConstraintLZ", {
+          url: "http://exac.broadinstitute.org/api/constraint",
+        }])
+        .add("recomb", ["RecombLZ", {
+          url: "http://portaldev.sph.umich.edu/api/v1/annotation/recomb/results/",
+        }])
+        .add("ld", ["LDServer", {
+          url: "https://portaldev.sph.umich.edu/ld/" ,
+        }])
+        .add(namespace, ["AssociationLZ", {
+          url: `${baseUrl}`,
+        }])
 
-    // Layout - the description of the plot and how data is presented. We start with only the genes panel.
-  var namespace = "study_41"
-  var id = 41
-  var mods = {
-    namespace: { "default": namespace, "assoc": namespace, ld: "ld" },
-    id: namespace,
-    title: { text: "Author" + " " + 2013 + " - " + "adv" },
-    y_index: -1,
-  };
+      var mods = {
+        namespace: { "default": namespace, "assoc": namespace, ld: "ld" },
+        id: namespace,
+        title: { text: "Author" + " " + 2013 + " - " + "adv" },
+        y_index: -1,
+      };
 
-  const variantForPlot = "16:53828066:C:T";
-  
-  const position = 53828066;
+      var initial_layout = {
+        state: {
+          chr: 16,
+          start,
+          end,
+          ld_pop: "EUR",
+          genome_build: "GRCh37",
+          variant
+        },
+        responsive_resize: true,
+        max_region_scale: rangeDifference,     
+        aspect_ratio: 2,
+        dashboard: LocusZoom.Layouts.get("toolbar", "region_nav_plot"),
+        panels: [
+          LocusZoom.Layouts.get("panel", "genes"),
+          LocusZoom.Layouts.get("panel", "association", mods)
+        ]
+      };
 
-  var initial_layout = {
-    state: {
-      chr: 16,
-      variant: variantForPlot,
-      start: position - 200,
-      end: position + 200,
-      ld_pop: "EUR",
-      genome_build: "GRCh38"
-    },
-  responsive_resize: true,
-  min_region_scale: 10000000,
-  // max_region_scale: 10000,
-  aspect_ratio: 4,
-  dashboard: LocusZoom.Layouts.get("toolbar", "region_nav_plot"),
-  panels: [
-    LocusZoom.Layouts.get("panel","genes"),
-    LocusZoom.Layouts.get("panel", "association", mods)
-  ]};
+      initial_layout.panels[0].toolbar.widgets.splice(0, 1);
+      initial_layout.panels[0].toolbar.widgets[0].group_position = "end";
 
-  initial_layout.panels[0].toolbar.widgets.splice(0,1);
-  initial_layout.panels[0].toolbar.widgets[0].group_position = "end";
-  // initial_layout.panels[1].curtain.show("Loading Study...", { "text-align": "center" });
-  // initial_layout.panels[1].on("data_rendered", function(){
-  //     this.curtain.hide();
-  //     this.legend.render();
-  // });
+      const plot = LocusZoom.populate(plotRef.current, data_sources, initial_layout);
+      plot.on("layout_changed", function () {
+        inputRef.current.value = plot.state.chr + ":" + plot.state.start + "-" + plot.state.end;
+      });
 
-  const plot = LocusZoom.populate(plotRef.current, data_sources, initial_layout);
-  plot.on("layout_changed", function(){
-    inputRef.current.value = plot.state.chr + ":" + plot.state.start + "-" + plot.state.end;
-  });
-  
-   }
-    
-
-  }, []); // Empty dependency array to ensure it runs once after mount
+    }
+  }, [start, end, variant]);
 
   return (
     <div>
-      {/* LocusZoom container */}
       <div id="lz-plot" ref={plotRef}></div>
       <input id="input_region" ref={inputRef} type="text" placeholder="Navigate to Region" readOnly />
     </div>
